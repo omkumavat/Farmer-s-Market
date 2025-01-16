247bd3d8141455cae49db108e2095afa166cc137
Shivaji Gadekar <gadekarshivaji8767@gmail.com>
2025-01-16 19:15:24 +0530

    dfhn

--- Frontend/src/Components/ProductForm.js
+++ Frontend/src/Components/ProductForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
-import "../CSS/productform.css";
+import "../DashboardCSS/productform.css";
 import { useAuth } from "../Context/AuthContext";
 import axios from 'axios';
 
