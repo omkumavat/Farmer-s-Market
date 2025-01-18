cd2e38020ec7ae081cf4d57bfcf3a5a60c8eeb9a
omkumavat <omkumavat2004@gmail.com>
2025-01-18 11:34:18 +0530

    a

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,29 +1,19 @@
 import React from "react";
-import ReactDOM from "react-dom/client"; // Use the client module
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import ReactDOM from "react-dom/client"; // Import the ReactDOM client module
+import { AuthProvider } from "./Context/AuthContext";
 import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./Context/AuthContext.js";
-import { Provider } from "react-redux";
-import store from "./Context/Store"; // Import your Redux store
-import Sales from "./Components/Sales.js";
+import App from "./App";
+import "./index.css";
 
-// ReactDOM.render(<Sales />, document.getElementById('root'));
-const root = ReactDOM.createRoot(document.getElementById("root")); 
+// Create the root element
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <Provider store={store}> {/* Wrap your app in Provider with store */}
-    <AuthProvider>
-      <BrowserRouter>
-        <React.StrictMode>
+  <AuthProvider>
+    <BrowserRouter>
+      <React.StrictMode>
           <App />
-        </React.StrictMode>
-      </BrowserRouter>
-    </AuthProvider>
-  </Provider>
+      </React.StrictMode>
+    </BrowserRouter>
+  </AuthProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
